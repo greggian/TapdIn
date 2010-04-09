d5ec6bf50b584ff7c4dd982f1239a4461dc03059
Greg Gianforcaro <greggian@gmail.com>
2010-04-08 23:16:48 -0400

    Update HTMLEmitter to redirect back to referrer
    When the handler returns and HTTPResponse, the HTMLTemplateEmitter
    forwards back to the referrer if one is provided. This is to support
    delete and create and other such requests that dont always have an
    appropriate HTML representation.

--- app/emitters.py
+++ app/emitters.py
@@ -1,11 +1,16 @@
 import piston.emitters
-
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 class HTMLTemplateEmitter(piston.emitters.Emitter):
 	
 	def render(self, request):
+                if isinstance(self.data, HttpResponse):
+                        referrer = request.META.get('HTTP_REFERER')
+                        if referrer:
+                                return HttpResponseRedirect(referrer)
+
 		return render_to_response(
 			template_name = '%s.html' % self.handler.viewname.lower(),
 			dictionary = self.data,#self.construct(),
